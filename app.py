6da9c96aa0eaad061f66c1c2539ef82bfc086407
fbarrientos97 <fbarrientosj@gmail.com>
2024-09-24 17:06:38 -0500

    arreglo raro

--- app.py
+++ app.py
@@ -9,7 +9,11 @@ app = Flask(__name__)
 EXCEL_FILE = 'ladder_data.xlsx'
 
 # Database setup
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///ladder.db')
+uri = os.environ.get('DATABASE_URL', 'sqlite:///ladder.db')  # or other relevant config var
+if uri and uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = uri
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
